707b8887f25b99dbb11558a544634a56d2d90b36
admin <admin@DESKTOP-ODPVLGR>
2023-06-19 22:59:52 +0530

    Print string added.

--- Afresh/afreash/NewTest.java
+++ Afresh/afreash/NewTest.java
@@ -3,5 +3,6 @@ package afreash;
 public class NewTest {
 public static void main(String[] args) {
 	System.out.println("Hello-GoodMorning");
+	System.out.println("NewOne for Git Repositiory");
 }
 }
