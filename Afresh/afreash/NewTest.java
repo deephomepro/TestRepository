b2ee19bec0a700d8ba1cc1644806689ad604acc9
admin <admin@DESKTOP-ODPVLGR>
2023-06-19 23:04:48 +0530

    Added New Print string

--- Afresh/afreash/NewTest.java
+++ Afresh/afreash/NewTest.java
@@ -4,5 +4,6 @@ public class NewTest {
 public static void main(String[] args) {
 	System.out.println("Hello-GoodMorning");
 	System.out.println("NewOne for Git Repositiory");
+	System.out.println("HelloNew");
 }
 }
